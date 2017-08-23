3cf43e6d311cfabc4affc3d0baa7cd7502097406
tuxfux-hlp-notes <tuxfux-hlp-notes@gmail.com>
2017-08-23 08:15:56 +0530

    day2: Project1

--- project1/src/polls/urls.py
+++ project1/src/polls/urls.py
@@ -2,8 +2,17 @@ from django.conf.urls import url
 
 from . import views
 
+# https://docs.djangoproject.com/en/1.11/intro/tutorial03/#namespacing-url-names
+app_name='polls'
 urlpatterns = [
+ 	# ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
 
 
